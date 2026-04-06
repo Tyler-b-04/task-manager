bd38941333fe529d45024967cb2d3648d4074629
Tyler-B-04 <bremont@usf.edu>
2026-04-06 13:15:04 -0400

    Created tasklist.js

--- app/components/taskcard.js
+++ app/components/taskcard.js
@@ -4,7 +4,7 @@ export default function TaskCard({ title, done }) {
       <span
         className={done ? 'line-through text-gray-400' : 'text-gray-900'}
       >
-        {title}                       {/* {} escapes into JS */}
+        {title}                       {}
       </span>
       {done && <span className="text-green-600 text-xs font-bold">Done</span>}
     </div>

15699d62531eed6a36e10021cb2b246c4c705a60
Tyler-B-04 <bremont@usf.edu>
2026-04-06 13:30:31 -0400

    making toogle in taskcard file

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ export default function HomePage() {
   return (
     <main className="p-8">
       <h1 className="text-2xl font-bold mb-4">Task Manager</h1>
-      <TaskCard title="Buy milk"    done={false} />
-      <TaskCard title="Write tests" done={true}  />
-      <TaskCard title="Ship it"     done={false} />
+      <TaskCard title="Buy milk" done={false} id={undefined} onToggle={undefined} />
+      <TaskCard title="Write tests" done={true} id={undefined} onToggle={undefined}  />
+      <TaskCard title="Ship it" done={false} id={undefined} onToggle={undefined} />
     </main>
   );
 }
